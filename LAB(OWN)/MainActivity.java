77fbcad9fd70e2529507508d4555dcaa776dd805
ShraiLakshmanR <53818678+ShraiLakshmanR@users.noreply.github.com>
2020-02-18 12:55:46 +0530

    Create MainActivity.java

---
+++ LAB(OWN)/MainActivity.java
@@ -0,0 +1,178 @@
+package com.example.lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.text.Selection;
+import android.text.TextUtils;
+
+import android.view.View;
+
+import android.view.View.OnClickListener;
+
+import android.widget.Button;
+
+import android.widget.EditText;
+
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener
+
+{
+
+//Defining the Views
+
+    EditText Num1;    //variable for edit text1
+
+    EditText Num2;
+
+    Button Add;
+
+    Button Sub;
+
+    Button Mul;
+
+    Button Div;
+
+    Button Clr;
+
+    TextView Result;
+
+    int clr=0;
+
+    @Override
+
+    public void onCreate(Bundle savedInstanceState)
+
+    {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+//Referring the Views
+
+        Num1 = (EditText) findViewById(R.id.editText1);
+
+        Num2 = (EditText) findViewById(R.id.editText2);
+
+        Add = (Button) findViewById(R.id.Add);
+
+        Sub = (Button) findViewById(R.id.Sub);
+
+        Mul = (Button) findViewById(R.id.Mul);
+
+        Div = (Button) findViewById(R.id.Div);
+
+        Clr = (Button) findViewById(R.id.Clr);
+
+
+
+        Result = (TextView) findViewById(R.id.textView);
+
+// set a listener
+
+        Add.setOnClickListener(this);
+
+        Sub.setOnClickListener(this);
+
+        Mul.setOnClickListener(this);
+
+        Div.setOnClickListener(this);
+
+        Clr.setOnClickListener(this);
+
+    }
+
+    @Override
+
+    public void onClick (View v)
+
+    {
+
+        float num1 = 0;
+
+        float num2 = 0;
+
+        float result = 0;
+
+        String oper = "";
+
+// check if the fields are empty
+
+        if(TextUtils.isEmpty(Num1.getText().toString()) || TextUtils.isEmpty(Num2.getText().toString()))
+
+            return;
+
+// read EditText and fill variables with numbers
+
+        num1 = Float.parseFloat(Num1.getText().toString());
+
+        num2 = Float.parseFloat(Num2.getText().toString());
+
+// defines the button that has been clicked and performs the corresponding operation
+
+// write operation into oper, we will use it later for output
+
+        switch(v.getId())
+
+        {
+
+            case R.id.Add:                    //When you press the + button the id is compared if it is true it will enter that respective case
+
+            oper = "+";
+
+            result = num1 + num2;
+
+            break;
+
+            case R.id.Sub:
+
+                oper = "-";
+
+                result = num1 - num2;
+
+                break;
+
+            case R.id.Mul:
+
+                oper = "*";
+
+                result = num1 * num2;
+
+                break;
+
+            case R.id.Div:
+
+                oper = "/";
+
+                result = num1 / num2;
+
+                break;
+
+            case R.id.Clr:
+                Num1.setText(" ");
+                Num2.setText(" ");
+                Num1.setSelection(1);
+                clr=1;
+                break;
+
+            default:
+
+                break;
+
+        }
+
+// form the output line
+        if(clr!=1)
+        Result.setText(num1 + " "+ oper + " "+ num2 + " = "+ result);
+
+        else
+            Result.setText("Answer is");
+            clr=0;
+
+
+    }
+
+}
